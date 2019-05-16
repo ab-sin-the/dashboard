dda27b9f2f7b02ef06a14edc1b747bc22153c3bc
ab-sin-the <ab0sin1the2@gmail.com>
2019-05-16 20:38:56 +0800

    fix ntopng

--- rrd/view/portal/ntopng.py
+++ rrd/view/portal/ntopng.py
@@ -2,10 +2,13 @@ from rrd import app
 from flask import render_template, abort, request, url_for, redirect, g
 import datetime
 from rrd.model.portal.top_proc_recorder import TopProcRecorder
+import socket 
 
 
 @app.route('/ntopng')
 def ntopng_redirect():
-
-    return redirect("http://127.0.0.1:3000")
+    host_name = socket.gethostname() 
+    host_ip = socket.gethostbyname(host_name) 
+    redirect_addr = "http://" + host_ip + ":3000"
+    return redirect(redirect_addr)
 
