1196c42f7d9ab982715f6eaf0af40f1cc50b056c
ab-sin-the <ab0sin1the2@gmail.com>
2019-05-18 16:11:04 +0800

    fix security audit bug

--- rrd/static/js/portal_audit.js
+++ rrd/static/js/portal_audit.js
@@ -6,12 +6,32 @@ function start_audit(script_name) {
             err_message_quietly(ret.msg);
             return;
         }
-
+        if (ret.data === "is running") {
+            document.getElementById("audit-result").innerText = script_name + "已经在运行了。";
+        }
         // display result
-        document.getElementById("audit-result").innerText = ret.data;
     }).error(function(req, ret, errorThrown){
         $(".loading").hide();
         err_message_quietly(req.statusText)
     })
 
+}
+
+
+function get_report(script_name){
+    $.getJSON("/api/security_report", {audit_method:script_name}, function(ret){
+        $(".loading").hide();
+        if (!ret.ok) {
+            err_message_quietly(ret.msg);
+            return;
+        }
+        if (ret.data === "") {
+            ret.data = "未执行过" + script_name  + "或者仍在运行中。";
+        }
+        document.getElementById("audit-result").innerText = ret.data;
+        // display result
+    }).error(function(req, ret, errorThrown){
+        $(".loading").hide();
+        err_message_quietly(req.statusText)
+    })
 }
--- rrd/view/portal/security_audit.py
+++ rrd/view/portal/security_audit.py
@@ -20,9 +20,18 @@ from flask import request, g, render_template, jsonify, abort
 import json
 import time
 import commands
+import subprocess
+import os
 
-lynis_workdir = "/home/absinthe/Projects/lynis"
-GScan_workdir = "/home/absinthe/Projects/GScan"
+lynis_workdir = "/home/work/open-falcon/lynis"
+GScan_workdir = "/home/work/open-falcon/GScan"
+
+def check_running(script_name):
+    command = "ps -ef | grep " + script_name + " | grep -v grep | wc -l"
+    proc_num = commands.getoutput(command)
+    if int(proc_num) != 0:
+        return True
+    return False
 
 @app.route('/portal/security_audit')
 def security_audit_get():
@@ -32,7 +41,36 @@ def security_audit_get():
 
 
 @app.route('/api/security_audit')
-def execute_audit():    
+def execute_audit():
+    ret = {
+            "ok": False,
+            "data": "",
+            }
+    ret["data"] = ""
+
+
+    try:
+        audit_method = request.args.get("audit_method")
+        if check_running(audit_method) == True:
+            ret['ok'] = True
+            ret["data"] = "is running"
+            return json.dumps(ret)
+        if audit_method == "lynis":
+            command = "cd " + lynis_workdir + " && ./lynis audit system  | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|C]//g' > report/lynis_report"
+            print(command)
+            subprocess.Popen(command, shell=True)
+        elif audit_method == "GScan":
+            command = "cd " + GScan_workdir + " && python GScan.py  | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|C]//g' > report/GScan_report"
+            print(command)
+            subprocess.Popen(command, shell=True)
+        ret['ok'] = True
+        return json.dumps(ret)
+    except Exception as e:
+        abort(400, str(ret))
+
+
+@app.route('/api/security_report')
+def get_report():
     ret = {
             "ok": False,
             "data": "",
@@ -40,16 +78,15 @@ def execute_audit():
     try:
         audit_method = request.args.get("audit_method")
         if audit_method == "lynis":
-            command = "cd " + lynis_workdir + " && ./lynis audit system  | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|C]//g'"
+            command = "cat " + lynis_workdir + "/report/lynis_report"
             print(command)
-            command_result = commands.getoutput(command)
+            command_res = commands.getoutput(command)
         elif audit_method == "GScan":
-            command = "cd " + GScan_workdir + " && python GScan.py  | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|C]//g'"
+            command = "cat " + GScan_workdir + "/report/GScan_report"
             print(command)
-            command_result = commands.getoutput(command)
-        ret["data"] = command_result
+            command_res = commands.getoutput(command) 
+        ret["data"] = command_res
         ret['ok'] = True
         return json.dumps(ret)
     except Exception as e:
         abort(400, str(ret))
-
